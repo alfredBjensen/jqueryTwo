0473a39984d6cfaa44d51bcf4fa4a11b1bbcab42
alfredBjensen <alfredBjensen@gmail.com>
2017-11-21 14:04:24 -0600

    updated css

--- scripts/our_script.js
+++ scripts/our_script.js
@@ -1,3 +1,19 @@
 $( document ).ready(function(){
-
+    hideElements();
 });
+
+function drawHeaderLine(){
+  var theLine = $("#mainHeader");
+  addLine(theLine, 'headerLine');
+}
+
+function hideElements(){
+  $("#mainHeader h1").hide();
+}
+
+
+function addLine(theElement, id){
+  var line = "<div id='" + id + "' class='basic-line hidden-line'></div>";
+  theElement.append($( line ));
+  alert('hello world');
+}
