0b4d968332473f0ceabe07f0652e4aea32c8533d
alfredBjensen <alfredBjensen@gmail.com>
2017-11-21 14:50:12 -0600

    update

--- scripts/our_script.js
+++ scripts/our_script.js
@@ -1,19 +1,37 @@
 $( document ).ready(function(){
     hideElements();
+    showHeader();
+    changeMain();
 });
+
+
+function changeMain(){
+  $("main").show("drop", {direction: "down"}, 1500);
+}
 
 function drawHeaderLine(){
   var theLine = $("#mainHeader");
   addLine(theLine, 'headerLine');
+  $("#headerLine").animate({width:"90%"}, 1500);
 }
 
 function hideElements(){
   $("#mainHeader h1").hide();
+  $("main").hide();
+}
+
+function showHeader(){
+  $("#mainHeader h1").fadeIn(1500);
+
+  //setTimeout will pause
+  setTimeout(function(){
+    drawHeaderLine();
+  }, 1000);
+
 }
 
 
 function addLine(theElement, id){
   var line = "<div id='" + id + "' class='basic-line hidden-line'></div>";
   theElement.append($( line ));
-  alert('hello world');
 }
